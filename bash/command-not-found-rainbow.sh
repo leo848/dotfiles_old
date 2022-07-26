1eb2a3665333af186a46f60827ebe76223baab35
leo <leonardo.blume@gymnasium-essen-werden.de>
2022-07-26 20:48:17 +0200

    starship config

--- bash/command-not-found-rainbow.sh
+++ bash/command-not-found-rainbow.sh
@@ -1,7 +1,9 @@
+#!/bin/bash
+
 command_not_found_handle () {
     if [ -x /usr/lib/command-not-found ]; then
-        /usr/bin/python3.8 /usr/lib/command-not-found -- $1 |& gay;
-        return $?;
+        /usr/bin/python3.8 /usr/lib/command-not-found -- "$1" |& gay --gay;
+        return 127;
     else
         return 127;
     fi
